85e512ff1fb7967b1d7057c34f917a1e39d172db
HuangDz <Dzreal_93@126.com>
2019-07-26 22:58:32 +0800

    脚本更新

--- blog/management/commands/create_articles.py
+++ blog/management/commands/create_articles.py
@@ -17,16 +17,16 @@ import os
 import time
 import pprint
 
-# from django.conf import settings
-# from django.core.management.base import BaseCommand
-# from blog.models import Article, Tag, Category
-# from django.contrib.auth import get_user_model
-# from django.core.exceptions import ObjectDoesNotExist
-# import datetime
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from blog.models import Article, Tag, Category
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
+import datetime
 
 
-# category_dir = settings.UPLOAD_ARTICLES_ROOT
-category_dir = '/Users/zhenwenlei/PycharmProjects/django_blog_dzreal/uploads/articles'
+category_dir = settings.UPLOAD_ARTICLES_ROOT
+# category_dir = '/Users/zhenwenlei/PycharmProjects/django_blog_dzreal/uploads/articles'
 category_li = os.listdir(category_dir)
 article_dict = {}
 
@@ -38,39 +38,39 @@ for category_name in category_li:
         article_filepath = os.path.join(article_dir, article_name)
         article_dict[category_name].append(article_filepath)
 
-pprint.pprint(article_dict)
+# pprint.pprint(article_dict)
 
 
 
-# class Command(BaseCommand):
-#     help = 'bulk create article'
-#
-#     def handle(self, *args, **options):
-#         save_article_list = set()
-#
-#         # 存文章
-#         for category_name, article_fliepath_li in article_dict.items():
-#             for article_filepath in article_fliepath_li:
-#                 article_name = article_filepath.split('/')[-1]
-#                 article_name_no_tail = article_name.split('.')[0]
-#                 # 不允许存在相同标题的文章
-#                 if(article_name_no_tail in save_article_list):
-#                     self.stdout.write(self.style.ERROR('{} 存储失败，因为有重复的文章\n'.format(article_name_no_tail)))
-#                     continue
-#                 with open(article_filepath, 'r', encoding='utf-8') as fp:
-#                     body = fp.read()
-#                     user = get_user_model().objects.get_or_create(email='dzreal_93@126.com', username='文小蕾',
-#                                                                   password='test!q@w#eTYU')[0]
-#                     category = Category.objects.get_or_create(name=category_name, parent_category=None)[0]
-#                     category.save()
-#                     article = Article.objects.get_or_create(category=category,
-#                                                             title=article_name_no_tail,
-#                                                             body=body,
-#                                                             author=user
-#                                                             )[0]
-#                     article.save()
-#                     self.stdout.write(self.style.SUCCESS('{} 文章保存成功 \n'.format(article_name_no_tail)))
-#                     save_article_list.add(article_name_no_tail)
-#                     from DjangoBlog.utils import cache
-#                     cache.clear()
-#         self.stdout.write(self.style.SUCCESS('请确认需要保存的文章 {}\n'.format(save_article_list)))
+class Command(BaseCommand):
+    help = 'bulk create article'
+
+    def handle(self, *args, **options):
+        save_article_list = set()
+
+        # 存文章
+        for category_name, article_fliepath_li in article_dict.items():
+            for article_filepath in article_fliepath_li:
+                article_name = article_filepath.split('/')[-1]
+                article_name_no_tail = article_name.split('.')[0]
+                # 不允许存在相同标题的文章
+                if(article_name_no_tail in save_article_list):
+                    self.stdout.write(self.style.ERROR('{} 存储失败，因为有重复的文章\n'.format(article_name_no_tail)))
+                    continue
+                with open(article_filepath, 'r', encoding='utf-8') as fp:
+                    body = fp.read()
+                    user = get_user_model().objects.get_or_create(email='dzreal_93@126.com', username='文小蕾',
+                                                                  password='test!q@w#eTYU')[0]
+                    category = Category.objects.get_or_create(name=category_name, parent_category=None)[0]
+                    category.save()
+                    article = Article.objects.get_or_create(category=category,
+                                                            title=article_name_no_tail,
+                                                            body=body,
+                                                            author=user
+                                                            )[0]
+                    article.save()
+                    self.stdout.write(self.style.SUCCESS('{} 文章保存成功 \n'.format(article_name_no_tail)))
+                    save_article_list.add(article_name_no_tail)
+                    from DjangoBlog.utils import cache
+                    cache.clear()
+        self.stdout.write(self.style.SUCCESS('请确认需要保存的文章 {}\n'.format(save_article_list)))
