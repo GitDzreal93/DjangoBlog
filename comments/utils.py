aeddaafdf43ca312cbe166aa2f8eaa1dae1ed501
HuangDz <Dzreal_93@126.com>
2019-08-03 17:07:40 +0800

    转化语言成西语

--- comments/utils.py
+++ comments/utils.py
@@ -22,26 +22,45 @@ logger = logging.getLogger(__name__)
 
 def send_comment_email(comment):
     site = get_current_site().domain
-    subject = '感谢您发表的评论'
+    # subject = '感谢您发表的评论'
+    subject = 'Gracias por tu comentario'
     article_url = "https://{site}{path}".format(site=site, path=comment.article.get_absolute_url())
+    # html_content = """
+    #                <p>非常感谢您在本站发表评论</p>
+    #                您可以访问
+    #                <a href="%s" rel="bookmark">%s</a>
+    #                来查看您的评论，
+    #                再次感谢您！
+    #                <br />
+    #                如果上面链接无法打开，请将此链接复制至浏览器。
+    #                %s
+    #                """ % (article_url, comment.article.title, article_url)
     html_content = """
-                   <p>非常感谢您在本站发表评论</p>
-                   您可以访问
+                   <p>Gracias por tu comentario</p>
+                   Puedes visitar
                    <a href="%s" rel="bookmark">%s</a>
-                   来查看您的评论，
-                   再次感谢您！
+                   Para ver tu comentario，
+                   Gracias de nuevo!
                    <br />
-                   如果上面链接无法打开，请将此链接复制至浏览器。
+                   Si el enlace anterior no se abre, copie este enlace a su navegador.
                    %s
                    """ % (article_url, comment.article.title, article_url)
     tomail = comment.author.email
     send_email([tomail], subject, html_content)
     try:
         if comment.parent_comment:
+            # html_content = """
+            #         您在 <a href="%s" rel="bookmark">%s</a> 的评论 <br/> %s <br/> 收到回复啦.快去看看吧
+            #         <br/>
+            #         如果上面链接无法打开，请将此链接复制至浏览器。
+            #         %s
+            #         """ % (article_url, comment.article.title, comment.parent_comment.body, article_url)
             html_content = """
-                    您在 <a href="%s" rel="bookmark">%s</a> 的评论 <br/> %s <br/> 收到回复啦.快去看看吧
-                    <br/>
-                    如果上面链接无法打开，请将此链接复制至浏览器。
+                    Estas en <a href="%s" rel="bookmark">%s</a> Comentario
+                    <br/> %s <br/> 
+                    Recibí una respuesta. Ve a verla.
+                    <br/>           
+                    Si el enlace anterior no se abre, copie este enlace a su navegador.
                     %s
                     """ % (article_url, comment.article.title, comment.parent_comment.body, article_url)
             tomail = comment.parent_comment.author.email
